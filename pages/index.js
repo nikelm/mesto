2c43eef912a141f7417375decf0faef036e7bc58
Nikolay Elmanov <nikelm@yandex.ru>
2020-07-29 21:11:13 +0300

    feat: сделать кнопку открытия popup

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+
+let element = document.querySelector('.profile__link');
+  element.addEventListener('click', function () {
+    let popup = document.querySelector('.popup');
+    popup.classList.add('popup_opened');
+});
+
+
